d484a29534c298997af43f92ee274a52e9e62951
Ashok kumar <112111053+Ashokali@users.noreply.github.com>
2024-05-02 16:03:17 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <h1 className="main-head animate-bounce">Hello, Click the Button !</h1>
+        <button className="hello-btn">Click Me</button>
+      </div>
+    </>
+  )
+}
+
+export default App
