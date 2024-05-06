f6f32cf2252528bee8ef202b858458b0fc47c6fc
Ashokali <ashokalisaras@gmail.com>
2024-05-06 18:22:17 +0530

    feat: file edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
+      <Header />
       <div className="main">
-        <h1 className="main-head animate-bounce">Hello, Click the Button !</h1>
-        <button className="hello-btn">Click Me</button>
+        {/* <h1 className="main-head animate-bounce">Hello, Click the Button !</h1>
+        <button className="hello-btn">Click Me</button> */}
+      </div>
+      <div className="main-1">
+
+      </div>
+      <div className="main-1">
+
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Sugar } from 'react-preloaders'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Sugar color={'#fafafa'} background="#6366f1" time={3000} animation="slide-down"  />
   </React.StrictMode>,
 )
